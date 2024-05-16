87d2aa6f4f9f725e73ba08ac86b597102fdff93f
klajdikolaj <160898891+klajdikolaj@users.noreply.github.com>
2024-05-16 08:39:22 +0200

    daisyui-installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,22 +51,22 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
-      <body className="bg-black loading">
+    <html lang="en"> 
+      <body>
         <SupabaseClientProvider>
-        <UserProvider>
-        <Navbar />
-        <main
-          id="skip"
-          className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
-        >
-          {children}
-        </main>
-        <Footer />
-        <Suspense>
-          <Toaster />
-        </Suspense>
-        </UserProvider>
+          <UserProvider>
+            <Navbar />
+            <main
+              id="skip"
+              className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+            >
+              {children}
+            </main>
+            <Footer />
+            <Suspense>
+              <Toaster />
+            </Suspense>
+          </UserProvider>
         </SupabaseClientProvider>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class', '[data-theme="dark"]'],
+  // darkMode: ['class', '[data-theme="dark"]'],
   content: [
     'app/**/*.{ts,tsx}',
     'components/**/*.{ts,tsx}',
@@ -36,5 +36,17 @@ module.exports = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate')]
+  plugins: [require('tailwindcss-animate'), require('daisyui')],
+
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "light", // name of one of the included themes for dark mode
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
